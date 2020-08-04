b5ed066720137b301acaa3310937d4f6dbbd9fb9
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-08-04 19:31:26 +0700

    root app with theme control

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement, useState, SetStateAction } from 'react'
+import "./App.css"
+import Covid from './components/Covid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "leaflet/dist/leaflet.css"
+import { createMuiTheme, ThemeProvider, Paper, makeStyles } from '@material-ui/core'
+
+export const DarkThemeContext = React.createContext<boolean>(false)
+export const SetDarkThemeContext = React.createContext<React.Dispatch<SetStateAction<boolean>> | undefined>(undefined)
+
+const useStyles = makeStyles(() => ({
+    app: {
+        height: "100vh"
+    }
+}))
+
+export default function App(): ReactElement {
+    const classes = useStyles()
+    const [darkTheme, setDarkTheme] = useState<boolean>(false)
+    const theme = createMuiTheme({
+        palette: {
+            type: darkTheme ? "dark" : "light"
+        }
+    })
+
+    return (
+        <ThemeProvider theme={theme}>
+            <DarkThemeContext.Provider value={darkTheme}>
+                <SetDarkThemeContext.Provider value={setDarkTheme}>
+                    <Paper className="App" elevation={0} style={{ minHeight: "100vh" }}>
+                        <Covid />
+                    </Paper>
+                </SetDarkThemeContext.Provider>
+            </DarkThemeContext.Provider>
+        </ThemeProvider>
+    )
 }
-
-export default App;
