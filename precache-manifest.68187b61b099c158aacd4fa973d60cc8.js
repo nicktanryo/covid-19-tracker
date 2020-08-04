072633632228b7be30ce69f407dc3b0b9bed70aa
Nicholas Tanryo <nick37tanryo@gmail.com>
2020-08-04 19:46:43 +0700

    Updates

---
+++ precache-manifest.68187b61b099c158aacd4fa973d60cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd5127f9b80563d01201715b04de512",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "49d52fabd34ccdf81acc",
+    "url": "/covid-19-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d769a91e88e9d188349d",
+    "url": "/covid-19-tracker/static/css/main.bc79afde.chunk.css"
+  },
+  {
+    "revision": "49d52fabd34ccdf81acc",
+    "url": "/covid-19-tracker/static/js/2.0f9df9d1.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-tracker/static/js/2.0f9df9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d769a91e88e9d188349d",
+    "url": "/covid-19-tracker/static/js/main.5be1ce76.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
